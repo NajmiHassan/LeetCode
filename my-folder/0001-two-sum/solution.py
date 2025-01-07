4fcd17abf99b1f713bb8fd52e615f906ab7c5b4b
NajmiHassan <130096761+NajmiHassan@users.noreply.github.com>
2025-01-07 19:30:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (24.63%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -8,16 +8,3 @@ class Solution:
                 return [prevMap[diff], i]
             prevMap[n]= i
         return
-        
-        # numMap = {}
-        # n = len(nums)
-
-        # for i in range(n):
-        #     numMap[nums[i]] = i
-
-        # for i in range(n):
-        #     complement = target - nums[i]
-        #     if complement in numMap and numMap[complement]!=i:
-        #         return [i, numMap[complement]]
-        
-        # return []

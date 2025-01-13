4708a6dfed3e159927c6f63b2d91d3c8db08cb15
NajmiHassan <130096761+NajmiHassan@users.noreply.github.com>
2025-01-13 18:07:57 +0000

    [LeetCode Sync] Runtime - 12 ms (40.65%), Memory - 31.5 MB (54.23%)

--- my-folder/0217-contains-duplicate/solution.py
+++ my-folder/0217-contains-duplicate/solution.py
@@ -1,21 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #hashset
-        #time complexity--->O(n)
-        #space complexity--->O(n)
-        hashset = set()
+        seen = set()
 
-        for n in nums:            
-            if n in hashset:
+        for i in nums:
+            if i in seen:
                 return True
-            hashset.add(n)
-        return False
-
-
-        #brute force
-        #time complexity--->O(n^2)
-        #space complexity--->O(1)
-        #sorting
-        #time complexity--->O(nlogn)
-        #space complexity--->O(1)
+            else:
+                seen.add(i)
         
+        return False

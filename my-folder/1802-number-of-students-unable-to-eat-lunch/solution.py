011c8fed7b0a58415efe68615097a7c1f292c657
NajmiHassan <130096761+NajmiHassan@users.noreply.github.com>
2025-01-23 14:53:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (19.75%)

--- my-folder/1802-number-of-students-unable-to-eat-lunch/solution.py
+++ my-folder/1802-number-of-students-unable-to-eat-lunch/solution.py
@@ -1,36 +1,19 @@
 class Solution:
-    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:       
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count = 0
+        while len(students) != 0:
+
+            if students[0] == sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
+                count = 0
+            
+            else:
+
+                count += 1
+                students.append(students.pop(0))
+
+            if count == len(students):
+                return count
         
-        preference_count = {0: 0, 1: 0}
-        for student in students:
-            preference_count[student] += 1
-
-        # Iterate over the sandwiches stack
-        for sandwich in sandwiches:
-            if preference_count[sandwich] == 0:
-                break
-            preference_count[sandwich] -= 1
-
-        # Return the remaining count of students who couldn't eat
-        return preference_count[0] + preference_count[1]
-
-
-
-
-        # counts = [0, 0]
-        # for student in students:
-        #     counts[student] += 1
-
-        # remaining = len(sandwiches)
-        # for sandwich in sandwiches:
-        #     if counts[sandwich] == 0:
-        #         break
-        #     if remaining == 0:
-        #         break
-        #     counts[sandwich] -= 1
-        #     remaining -= 1
-        
-        # return remaining
-
-
-
+        return 0

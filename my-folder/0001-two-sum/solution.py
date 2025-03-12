0fd2e5cfffaf422f4809b2f9d4ba0ed358ee2355
NajmiHassan <130096761+NajmiHassan@users.noreply.github.com>
2025-03-12 11:52:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (6.45%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,10 +1,39 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {} #val : index
+        
+        indexed_nums = [(nums[i], i) for i in range(len(nums))] 
+        
+        indexed_nums.sort()
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n]= i
-        return
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            current_sum = indexed_nums[left][0] + indexed_nums[right][0]
+            if current_sum == target:
+                return [indexed_nums[left][1], indexed_nums[right][1]]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+        return []
+
+
+    #   prevMap = {} #val : index
+
+    #     for i, n in enumerate(nums):
+    #         diff = target - n
+    #         if diff in prevMap:
+    #             return [prevMap[diff], i]
+    #         prevMap[n]= i
+    #     return
+
+    #       prevMap = {} #val : index
+
+        # for i, n in enumerate(nums):
+        #     diff = target - n
+        #     if diff in prevMap:
+        #         return [prevMap[diff], i]
+        #     prevMap[n]= i
+        # return
+
+

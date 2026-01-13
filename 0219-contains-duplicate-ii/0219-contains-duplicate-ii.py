79f11dc050f6a22eab61afa8660501cb7cbd1c49
Najmi Hassan <130096761+NajmiHassan@users.noreply.github.com>
2026-01-14 03:10:58 +0500

    Update 0219-contains-duplicate-ii.py

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,5 +1,23 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Sliding Window Solution
+        window = set()
+
+        for i in range(len(nums)):
+            if nums[i] in window:
+                return True
+            
+            window.add(nums[i])
+
+            if len(window) > k:
+                window.remove(nums[i - k])
+
+        return False
+
+        
+
+        
+        # Hash Map Solution
         seen = {}
 
         for i, num in enumerate(nums):

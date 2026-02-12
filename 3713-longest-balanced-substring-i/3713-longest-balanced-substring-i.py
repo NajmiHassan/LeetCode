079da201a6a7a3a62ccfa45f89d98577ef04d136
Najmi Hassan <130096761+NajmiHassan@users.noreply.github.com>
2026-02-13 01:24:05 +0500

    Time: 3006 ms (40.06%), Space: 19.5 MB (22.02%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        max_len = 0
+
+        for i in range(n):
+            counts = {}
+            for j in range(i, n):
+                char = s[j]
+                counts[char] = counts.get(char, 0) + 1
+
+                freqs = list(counts.values())
+                if len(set(freqs)) == 1:
+                    max_len = max(max_len, j - i + 1)
+        return max_len
+
+
+

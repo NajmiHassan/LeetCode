284626092777012c91cd5d091b610275578168d5
Najmi Hassan <130096761+NajmiHassan@users.noreply.github.com>
2026-02-21 17:15:45 +0500

    Update 0762-prime-number-of-set-bits-in-binary-representation.py

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
@@ -1,5 +1,6 @@
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
+        # with custom helper function
         prime = {2, 3, 5, 7, 11, 13, 17, 19}
         ans = 0
 
@@ -16,4 +17,16 @@ class Solution:
             if set_bits in prime:
                 ans += 1
 
+        return ans
+
+# without helper fuction
+    primes = {2, 3, 5, 7, 11 , 13, 17, 19}
+        ans = 0
+
+        for num in range(left, right + 1):
+            set_bits = num.bit_count()
+
+            if set_bits in primes:
+                ans += 1
+        
         return ans

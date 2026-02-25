0cf3902f7193df2700d9b09741534501862d5379
Najmi Hassan <130096761+NajmiHassan@users.noreply.github.com>
2026-02-25 23:47:00 +0500

    Update 1356-sort-integers-by-the-number-of-1-bits.py

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
+        # with custom count function
         def count_set_bits(n):
             count = 0
             while n > 0:
@@ -9,4 +10,8 @@ class Solution:
 
         arr.sort(key = lambda x: (count_set_bits(x), x))
 
+        return arr
+
+# using built-in funcution
+        arr.sort(key=lambda x : (x.bit_count(),x))
         return arr
